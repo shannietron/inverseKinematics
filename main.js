0a24a203796514a5177a9326b3b8b0f52535c3d9
Shanmugam MPl <git@shan.mp>
2017-05-24 02:38:22 -0400

    Update frame and calculate new coordinates accordingly

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 window.addEventListener("load", function(){
 	var canvas = document.getElementById('canvas');
+	context = canvas.getContext('2d')
 	width = window.innerWidth;
 	height = window.innerHeight;
 	canvas.width = width;
 	canvas.height= height;
 	
-	function draw(){
-	if (canvas.getContext) {
-     //    var ctx = canvas.getContext('2d');
-     //    ctx.beginPath();
-    	// ctx.moveTo(0, 0);
-    	// ctx.lineTo(width/2, height/2);
-    	// ctx.stroke();
-      }
-  }
 
-segment1 = new segment(100,100,100,Math.PI);
-segment2 = new segment(100,100,100,Math.PI/2);
+	segment1 = new segment(100,100,100,Math.PI);
+	segment2 = new segment(segment1.EndX,segment1.EndY,100,Math.PI/2);
 
-segment1.render(canvas.getContext('2d'));
-segment2.render(canvas.getContext('2d'));
+	updateFrame();
 
-draw();
+	function updateFrame(){
+		context.clearRect(0,0,width,height);
+		segment1.angle += 0.1;
+		segment2.x = segment1.EndX;
+		segment2.y = segment1.EndY;
+		segment1.render(context);
+		segment2.render(context);
+		requestAnimationFrame(updateFrame);
+	}
 });
